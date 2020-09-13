b71af3a32aaa4808960ea634cb758019a223a282
Nisa Khusong <n.khusong@gmail.com>
2020-09-13 15:19:03 +0700

    feat: init to-do layout with Bootstrap

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} lg={6}>
+            <h4 className="todo-header">To-Do</h4>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} lg={6}>
+            <Form className="todo-form">
+              <Form.Row>
+                <Col xs={9}>
+                  <Form.Control placeholder="Enter new to-do" />
+                </Col>
+                <Col xs={3}>
+                  <Button block>Add</Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10} lg={6}>
+            <ListGroup>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+              <ListGroup.Item>test</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
