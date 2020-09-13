7eeb0b79edb1aa37ff11c7e4caccce439d8c4921
Nisa Khusong <n.khusong@gmail.com>
2020-09-13 17:47:21 +0700

    fix: cors problem

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import methodOverride from 'method-override'
+import cors from 'cors'
+
 import db from './src/models/index.js'
 import todo from './src/controllers/todo.js'
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -33,5 +36,4 @@ app.use((err,req, res, next) => {
 })
 
 const server = app.listen(3333, () => {})
-
 export default server
